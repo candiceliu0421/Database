407a568965f26912aa0defcfdb663e6b876560a3
candiceliu0421 <candice102403006@gmail.com>
2018-11-13 23:49:21 +0800

    Update CombineTwoTables.sql

--- CombineTwoTables.sql
+++ CombineTwoTables.sql
@@ -20,6 +20,9 @@ Table: Address
 +-------------+---------+
 AddressId is the primary key column for this table.
 
+Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+
+FirstName, LastName, City, State
 
 
 # Write your MySQL query statement below
